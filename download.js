3b3235d328e5906963514915946356b2e52706b2
codefrydev <codefrydev@gmail.com>
2024-03-26 09:01:33 +0000

    Deploying to gh-pages from  @ ed0cbc11f4233efa9d3c5e2912f93be83ab8d177 🚀

---
+++ download.js
@@ -0,0 +1,11 @@
+window.downloadFile = (fileName, content, contentType) => {
+    const blob = new Blob([content], { type: contentType });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+};
